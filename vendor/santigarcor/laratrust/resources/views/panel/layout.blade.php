7202dde0c095893249ac6f980ec8ba933a2bc086
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-06-09 09:34:25 +0300

    Change admin navbar

--- vendor/santigarcor/laratrust/resources/views/panel/layout.blade.php
+++ vendor/santigarcor/laratrust/resources/views/panel/layout.blade.php
@@ -13,12 +13,10 @@
 </head>
 <body>
 <div>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background-color: #013676!important;">
         <div class="container">
-            <a class="navbar-brand d-flex" href="{{ route('home') }}">
-            <div><img src="{{ asset('/vendor/laratrust/img/logo.png') }}" style="height:40px; border-right: 1px solid #333;" class="pr-3" a></div>
-                <div class="pl-3 pt-2" style="color:#000;">{{ config('app.name', 'StratizenAsk') }}</div>
-            </a>
+            <a class="navbar-brand d-flex" href="{{ route('home') }}" style="background-color: #fff; border-radius: 50%; padding: 10px">
+            <div><img src="{{ asset('/vendor/laratrust/img/logo.png') }}" style="height:40px"></div></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -30,70 +28,57 @@
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link pt-3" href="{{ route('login') }}" style="color: #000; font-size:16px;">{{ __('Login') }}</a>
+                            <a class="nav-link pt-3" href="{{ route('login') }}" style="color: #fff; font-size:12.5px;">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link pt-3" href="{{ route('register') }}" style="color: #000; font-size:16px;">{{ __('Register') }}</a>
+                                <a class="nav-link pt-3" href="{{ route('register') }}" style="color: #fff; font-size:12.5px;">{{ __('Register') }}</a>
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown pt-2">
-
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle h6 pt-2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color:#000">
-                            <img src="{{ Storage::url('uploads/avatars/'. Auth::user()->id . '/' . Auth::user()->avatar . '') }}" class="rounded-circle pb-1 pr-1" style="height:30px;width:30px;float:left;" alt=""  onerror="this.src='uploads/avatars/avatar.png';">
-                                    {{ Auth::user()->username }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="">My Profile</a>
-                                <a class="dropdown-item" href="">Edit Profile</a>
-                                @role('administrator')
-                                <a class="dropdown-item" href="">Admin Panel</a>
-                                @endrole
-                                <a class="dropdown-item" href=""
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
 
                         <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
+                              My Profile
+                            </a>
+                          </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
+                              Edit Profile
+                            </a>
+                          </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Self Registration
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Coursework Marks
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Progress Reports
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Attendance
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Exam Card
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Fee Structure
                             </a>
                           </li>
                           <li class="nav-item">
-                            <a href="" class="nav-link pt-3 pl-4" style="color: #000; font-size:16px;">
+                            <a href="#" class="nav-link pt-3 pl-3" style="color: #fff; font-size:12.5px;">
                               Register for Graduation
                             </a>
                           </li>
