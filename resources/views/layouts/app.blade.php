cb1858444a4ba572b2c10cadb6e0870a4c1ee31c
Cyprian Kyalo <cypkyalo17@gmail.com>
2021-06-14 20:15:01 +0300

    App view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="shortcut icon" href="{{ asset('/vendor/laratrust/img/logo.png') }}">
 
-        <!-- Styles -->
+        {{-- Styles --}}
         <link href="{{ asset(mix('laratrust.css', 'vendor/laratrust')) }}" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
