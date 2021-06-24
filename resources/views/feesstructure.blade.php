d04414292f94c6c94e10c549abbb01d55008544d
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-06-24 17:41:49 +0300

    Change Fee Structure route

--- resources/views/feesstructure.blade.php
+++ resources/views/feesstructure.blade.php
@@ -67,7 +67,7 @@
         @endif
         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
             <div class="unit-section flex mt-3">
-                <a href="{{route('registration')}}"
+                <a href="{{route('feesstructure.index')}}"
                 class="self-end text-blue-700 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded registered" id="selected">
                     Fee Structures
                 </a>
