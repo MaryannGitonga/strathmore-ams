e51fd621a605a4c49f005f12551a707328698c6c
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-06-25 20:33:45 +0300

    Add blade templates for all modules

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -16,73 +16,8 @@ use Illuminate\Support\Facades\Log;
 
 class StudentController extends Controller
 {
-    // Module 4
-    public function profile()
-    {
-        $user = Auth::user()->id;
-        return view('dashboard', compact('user'));
-    }
-    public function progress()
-    {
-        $user = Auth::user()->id;
-        return view('report', compact('user'));
-    }
 
-    public function coursework(){
-        $user = Auth::user()->id;
-        $assessments = Assessment::all();
-        $scores = Score::all();
-        return view('coursemark', compact('user', 'assessments','scores',));
-    }
-
-    public function exam_card()
-    {
-        $user = Auth::user()->id;
-        return view('examcard', compact('user'));
-    }
-
-    // End of Module 4
-
-    // Module 2
-
-    public function register_units() {
-        $registered_units = Auth::user()->student->units()->where('status', 'pending')->get();
-        return view('registration',compact('registered_units'))->with('i', (request()->input('page', 1) - 1) * 10);
-    }
-
-    public function available_units() {
-        $units = Unit::all();
-        $available_units = array();
-        foreach ($units as $unit) {
-            if (DB::table('student_unit')->where('unit_id', $unit->id)->where('student_id', Auth::user()->student->id)->doesntExist()) {
-                array_push($available_units, $unit);
-            }
-        }
-
-        return view('available_units',compact('available_units'))->with('i', (request()->input('page', 1) - 1) * 10);
-    }
-
-    public function retakes() {
-        $units = Unit::all();
-        $retakes = array();
-        foreach ($units as $unit) {
-            if (DB::table('student_unit')->where('unit_id', $unit->id)->where('student_id', Auth::user()->student->id)->where('status', 'retake')->exists()) {
-                array_push($retakes, $unit);
-            }
-        }
-        return view('retake', compact('retakes'));
-    }
-
-    public function register_unit(Unit $unit)
-    {
-        $unit->students()->attach(Auth::user()->student->id);
-
-        return redirect()->route('available')->with('success', 'Unit registered successfully');
-    }
-
-    // End of Module 2
-
-    // Module 1
+    //////////////// Module 1 ////////////////
 
     public function personal_details()
     {
@@ -124,9 +59,53 @@ class StudentController extends Controller
         return redirect()->route('account.profile')->with('success', 'Personal files uploaded successfully');
     }
 
-    // End of Module 1
+    //////////////// End of Module 1 ////////////////
 
-    // Module 3
+
+
+    //////////////// Module 2 ////////////////
+
+    public function register_units() {
+        $registered_units = Auth::user()->student->units()->where('status', 'pending')->get();
+        return view('registration',compact('registered_units'))->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    public function available_units() {
+        $units = Unit::all();
+        $available_units = array();
+        foreach ($units as $unit) {
+            if (DB::table('student_unit')->where('unit_id', $unit->id)->where('student_id', Auth::user()->student->id)->doesntExist()) {
+                array_push($available_units, $unit);
+            }
+        }
+
+        return view('available_units',compact('available_units'))->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    public function retakes() {
+        $units = Unit::all();
+        $retakes = array();
+        foreach ($units as $unit) {
+            if (DB::table('student_unit')->where('unit_id', $unit->id)->where('student_id', Auth::user()->student->id)->where('status', 'retake')->exists()) {
+                array_push($retakes, $unit);
+            }
+        }
+        return view('retake', compact('retakes'));
+    }
+
+    public function register_unit(Unit $unit)
+    {
+        $unit->students()->attach(Auth::user()->student->id);
+
+        return redirect()->route('available')->with('success', 'Unit registered successfully');
+    }
+
+    //////////////// End of Module 2 ////////////////
+
+
+
+
+    //////////////// Module 3 ////////////////
 
     public function course_work()
     {
@@ -138,9 +117,38 @@ class StudentController extends Controller
         $pdf = App::make('dompdf.wrapper');
         $pdf->loadView('coursework_marks');
         Log::info(print_r($pdf->loadView('coursework_marks'), true));
-        return redirect()->route('coursework_marks');
-        // return $pdf->download('coursework-marks.pdf');
+        return $pdf->download('coursework-marks.pdf');
     }
 
-    // End of Module 3
+    //////////////// End of Module 3 ////////////////
+
+
+
+
+    //////////////// Module 4 ////////////////
+    public function profile()
+    {
+        $user = Auth::user()->id;
+        return view('dashboard', compact('user'));
+    }
+    public function progress()
+    {
+        $user = Auth::user()->id;
+        return view('report', compact('user'));
+    }
+
+    public function coursework(){
+        $user = Auth::user()->id;
+        $assessments = Assessment::all();
+        $scores = Score::all();
+        return view('coursemark', compact('user', 'assessments','scores',));
+    }
+
+    public function exam_card()
+    {
+        $user = Auth::user()->id;
+        return view('examcard', compact('user'));
+    }
+
+    //////////////// End of Module 4 ////////////////
 }
