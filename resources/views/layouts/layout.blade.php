f559fcbbd430a21a60d133d292fed3c273eab33e
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-07-29 12:53:21 +0300

    Remove vendor folder

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="shortcut icon" href="{{ asset('img/logo.png') }}">
   <title>Strathmore AMS - @yield('title')</title>
-  <link href="{{ asset(mix('laratrust.css')) }}" rel="stylesheet">
+  <link href="{{ asset('laratrust.css') }}" rel="stylesheet">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
